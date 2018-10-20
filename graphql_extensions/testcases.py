cf270be8ea460900232a2d70ecca7b432440e23d
mongkok <domake.io@gmail.com>
2018-10-21 00:24:22 +0700

    Added schema method to SchemaRequestFactory

--- graphql_extensions/testcases.py
+++ graphql_extensions/testcases.py
@@ -10,26 +10,21 @@ from .views import GraphQLView
 
 class SchemaRequestFactory(RequestFactory):
 
+    def __init__(self, **defaults):
+        super().__init__(**defaults)
+        self._schema = graphene_settings.SCHEMA
+
+    def schema(self, **kwargs):
+        self._schema = graphene.Schema(**kwargs)
+
     def execute(self, context, query, variables):
-        response = self._schema.execute(
+        return self._schema.execute(
             query,
             context=context,
             variables=variables)
 
-        if response.errors is not None:
-            response.errors = [
-                GraphQLView.format_error(error)
-                for error in response.errors
-            ]
-
-        return response
-
 
 class SchemaClient(SchemaRequestFactory, Client):
-
-    def __init__(self, **defaults):
-        super().__init__(**defaults)
-        self._schema = graphene_settings.SCHEMA
 
     def request(self, **request):
         request = WSGIRequest(self._base_environ(**request))
@@ -39,12 +34,17 @@ class SchemaClient(SchemaRequestFactory, Client):
             request.user = get_user(request)
         return request
 
-    def schema(self, **kwargs):
-        self._schema = graphene.Schema(**kwargs)
-
     def execute(self, query, variables=None, **headers):
         context = self.post('/', **headers)
-        return super().execute(context, query, variables)
+        response = super().execute(context, query, variables)
+
+        if response.errors is not None:
+            response.errors = [
+                GraphQLView.format_error(error)
+                for error in response.errors
+            ]
+
+        return response
 
 
 class SchemaTestCase(testcases.TestCase):
