51feaddf830e81c5226f71bd00605fc0b4bac6b6
mongkok <domake.io@gmail.com>
2021-06-04 01:26:35 +0700

    Renamed test class name

--- tests/test_lookups.py
+++ tests/test_lookups.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from graphql_extensions import lookups
 
 
-class LookupsTypesTests(TestCase):
+class LookupsTests(TestCase):
 
     def test_lookups(self):
         self.assertTrue(lookups.TEXT_LOOKUPS)
