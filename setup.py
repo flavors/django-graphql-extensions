10a4b6a804646e8ff4840889666519c2dd5eb64d
mongkok <domake.io@gmail.com>
2021-06-04 12:26:01 +0700

    Fixed graphene-django requirement

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     packages=find_packages(exclude=['tests*']),
     install_requires=[
         'Django>=2.0',
-        'graphene-django==3.0.0b1',
+        'graphene-django>=3.0.0b1',
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
